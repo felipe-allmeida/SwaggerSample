60d99f64800f72b8c5835759f8982f95b0a9bdaa
felipe-allmeida <felipe.allmeida.dev@gmail.com>
2020-05-26 00:58:37 -0300

    Creating swagger sample repository

---
+++ SwaggerSample/Controllers/v1/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerSample.Models;
+using System;
+using System.Collections.Generic;
+
+namespace SwaggerSample.Controllers.v1
+{
+    /// <summary>
+    /// Api Product, retorna e adiciona produtos
+    /// </summary>
+    [Route("v1/products")]
+    public class ProductController : Controller
+    {
+        private static readonly List<Product> _products = new List<Product>()
+        {
+            new Product("Copo", "Copo legal", 10.000M),
+            new Product("Moletom", "Moletom GAP preto", 25.000M),
+            new Product("Teckpix", "Nem preciso falar nada", 50.000M),
+        };
+
+        /// <summary>
+        /// Retorna a lista com todos os produtos
+        /// </summary>
+        /// <returns>products</returns>
+        [HttpGet]
+        [Route("")]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            return _products;
+        }
+
+        /// <summary>
+        /// Adiciona um novo produto
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="product">É um produto :D</param>
+        [HttpPost]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces("application/json")]
+        public IActionResult AddProduct([FromBody] Product product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException("The product cannot be null");
+            }
+
+            _products.Add(product);
+
+            return Ok(product);
+        }
+    }
+
+    
+}
---
+++ SwaggerSample/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerSample.Models
+{
+    public class Product
+    {
+        public Product() { }
+        public Product(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+        [Required]
+        public string Name { get; set; }
+        [DefaultValue("No description")]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
